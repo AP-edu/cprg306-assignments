d8ebdd179fa789edd4c32faf992098656caabc47
Alexander-Pacunayen <alexander.pacunayen@edu.sait.ca>
2026-02-06 22:29:48 -0700

    fixed production error

--- app/components/PageHeader.js
+++
@@ -1,8 +0,0 @@
-export default function PageHeader() {
-  return (
-    <header>
-      <h1>{title}</h1>
-      <h2>{description}</h2>
-    </header>
-  );
-}
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -1,11 +1,9 @@
 import { GlobalHeader } from "../components/GlobalHeader";
-import PageHeader from "../components/PageHeader";
 
 export default function Page() {
   return (
     <main>
       <GlobalHeader />
-      <PageHeader />
     </main>
   );
 }
